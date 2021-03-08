590b90b835cef424f82b699ea5c8875c3c497462
Victor Lenoir <victorlenoir@antescofo.com>
2021-03-08 09:42:00 +0100

    cont

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,16 @@
 import Websocket from './common/Websocket';
 import './App.css';
 
+function Home(props) {
+    console.log(props);
+    return 'Take me home';
+}
 function App() {
     return (
         <Websocket>
           <div className="main">
-            Now we just have to do something with our life
+            <div>Now we just have to do something with our life</div>
+            <Home/>
           </div>
         </Websocket>
     );
--- web/src/common/Websocket.js
+++ web/src/common/Websocket.js
@@ -1,24 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import config from './config';
 
 // This is ugly but it's just for POC leave me alone
 // Go through https://github.com/giantmachines/redux-websocket
 
 
+export const WEBSOCKET_STATE = {
+    INITIAL: "initial",
+    CONNECTING: "connecting",
+    CONNECTED: "connected",
+    CLOSE: "close"
+};
 
 function Websocket(props) {
-    useEffect(() => {
+    const [state, setState] = useState(WEBSOCKET_STATE.INITIAL);
+
+    const try_connecting = () => {
+        // If we are already connecting or connected we skip that block
+        if ((state === WEBSOCKET_STATE.CONNECTING) || (state === WEBSOCKET_STATE.CONNECTED)) return;
         const socket = new WebSocket(config.websocket_url, "echo-protocol");
+
         // Connection opened
         socket.addEventListener('open', function (event) {
             console.log('Websocket connected');
+            setState(WEBSOCKET_STATE.CONNECTED);
             // Tell my identity to the server
         });
 
         // Connection opened
         socket.addEventListener('close', function (event) {
             console.log('Websocket closed');
+            setState(WEBSOCKET_STATE.CLOSE);
+            window.setTimeout(try_connecting, 1000);  // We retry connecting in 1000ms
+            
             // We need to display a lost connection in APP and poll to retrieve connection to a new websocket =)
+
         });
 
         // Listen for messages
@@ -26,10 +42,24 @@ function Websocket(props) {
             console.log('Message from server ', event.data);
         });
         // const WebsocketContext = React.createContext({websocket: });
-    });
+
+    };
+    useEffect(() => {
+        try_connecting();
+    }, [props]);
+
+    let connection_banner = null;
+    if (state === WEBSOCKET_STATE.CONNECTING) {
+        connection_banner = <div>Reconnecting...</div>;
+    }
+    else if (state === WEBSOCKET_STATE.CLOSE) {
+        connection_banner = <div>Lost Internet connection... Retrying...</div>;
+
+    }
     return (<>
-              {props.children}
-            </>);
+       {connection_banner}
+       {props.children}
+     </>);
 }
 
 export default Websocket;
